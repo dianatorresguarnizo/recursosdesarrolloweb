1c79c7603900840d8fce6e3bc4f48332843e421c
utpl <utpl@MacBook-Pro-de-utpl.local>
2025-06-26 10:07:46 -0500

    Variables en PHP

---
+++ variables/index.php
@@ -0,0 +1,30 @@
+<?php
+// Declaración de Variables en PHP
+
+// Variable de tipo cadena (string)
+$nombre = "Juan";
+
+// Variable de tipo entero (integer)
+$edad = 30;
+
+// Variable de tipo flotante (float o double)
+$altura = 1.75;
+
+// Variable de tipo booleano (boolean)
+$esEstudiante = true;
+
+// Variable de tipo array (arreglo)
+$lenguajes = array("PHP", "JavaScript", "Python");
+
+// Variable de tipo null
+$valorNulo = null;
+
+// Mostramos las variables
+echo "Nombre: " . $nombre . "<br>";
+echo "Edad: " . $edad . "<br>";
+echo "Altura: " . $altura . " metros<br>";
+echo "¿Es estudiante?: " . ($esEstudiante ? "Sí" : "No") . "<br>";
+echo "Lenguajes de programación: " . implode(", ", $lenguajes) . "<br>";
+echo "Valor nulo: " . var_export($valorNulo, true) . "<br>";
+
+?>
